2b5fb0fa7f2478b91887f1a4b3f384c12fd202a1
Erik Dubois <erik.dubois@gmail.com>
2021-01-03 11:39:43 +0100

    new check for polybar

--- etc/skel/.config/cwm/scripts/check-polybar.sh
+++ etc/skel/.config/cwm/scripts/check-polybar.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
-message=$(cat ~/.xsession-errors | grep 'Disabling module "ewmh"')
-#echo $message
-process=$(pgrep -x polybar)
-#echo $process
 
+process=""
 
+#waiting for polybar to appear
 while [ -z "$process" ]
 do
 	sleep 1
 	process=$(pgrep -x polybar)
 done
 
+#check if errors exist
+message=$(cat ~/.xsession-errors | grep 'Disabling module "ewmh"')
+process=$(pgrep -x polybar)
 
+
+#what if errors exist
 if [ -z "$process" ] && [ -z "$message" ] ; then
 	echo "No polybar and no error"
 else
-	sleep 5
-	arcolinux-restart-polybar	
+	# Terminate already running bar instances
+	killall -q polybar
+
+	# Wait until the processes have been shut down
+	while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+	
+	# launch polybar again
+	sh ~/.config/polybar/launch.sh	
 fi
 
